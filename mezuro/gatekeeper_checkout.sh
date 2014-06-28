c410b7b3ad1207ca9cbe28b13f7dde10490ab026
Daniel Miranda <danielkza2@gmail.com>
2014-06-28 18:17:19 -0300

    Begin use of separate user for running Mezuro

---
+++ mezuro/gatekeeper_checkout.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+cd /home/mezuro
+git clone git@github.com:mezuro/kalibro_gatekeeper.git
+git checkout 2f9417
---
+++ mezuro/mezuro_checkout.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+cd /home/mezuro
+git clone git@github.com:mezuro/mezuro.git
+git checkout 907442b6e8
