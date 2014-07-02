33b8130400c1de8b4f106bd68e35f5011eed41fd
Daniel Miranda <danielkza2@gmail.com>
2014-07-02 14:38:01 -0300

    Update scripts for mezuro user

--- mezuro/gatekeeper_checkout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd /home/mezuro
-git clone https://github.com/mezuro/kalibro_gatekeeper.git
-git checkout 2f9417
--- mezuro/mezuro_checkout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd /home/mezuro
-git clone https://github.com/mezuro/mezuro.git
-git checkout 907442b6e8
